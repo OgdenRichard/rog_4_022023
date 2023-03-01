6af15bb451e160295eef63911499fbedb5331978
Richard Ogden <ogden.richard@gmail.com>
2023-03-01 10:42:26 +0100

    010323 | close modal click + esc

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const navbarBtn = document.querySelector('#navbar-menu');
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
+const modalCloseBtn = document.querySelectorAll('.close');
 const formData = document.querySelectorAll('.formData');
 
 // launch navbar menu
@@ -21,5 +22,22 @@ function launchModal() {
   modalbg.style.display = 'block';
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = 'none';
+}
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+
+// close modal
+modalCloseBtn.forEach((btn) => btn.addEventListener('click', () => {
+  modalbg.style.display = 'none';
+}));
+
+// close modal on keypress esc
+document.addEventListener("keydown", ({key}) => {
+  if (key === "Escape"){
+    modalbg.style.display = 'none';
+  } 
+});
